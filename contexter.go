7a67ad7f76955ce5a70e0049c21325e6e1f75be9
Aidan Steele <aidan.steele@glassechidna.com.au>
2019-10-30 15:41:19 +1100

    Change span name

--- contexter.go
+++ contexter.go
@@ -2,12 +2,13 @@ package awshoney
 
 import (
 	"context"
+	"fmt"
 	"github.com/glassechidna/awsctx"
 	"github.com/honeycombio/beeline-go"
 )
 
 func contexter(ctx context.Context, request *awsctx.AwsRequest, inner func(ctx context.Context)) {
-	ctx, span := beeline.StartSpan(ctx, "aws.api")
+	ctx, span := beeline.StartSpan(ctx, fmt.Sprintf("%s %s", request.Service, request.Action))
 	span.AddField("aws.service", request.Service)
 	span.AddField("aws.action", request.Action)
 
