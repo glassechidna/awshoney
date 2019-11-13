04e87a281b9f221ea207adb3c6e70940f7a8d74e
Aidan Steele <aidan.steele@glassechidna.com.au>
2019-11-13 12:32:47 +1100

    include aws.err.code and aws.err.message fields

--- contexter.go
+++ contexter.go
@@ -3,12 +3,15 @@ package awshoney
 import (
 	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/glassechidna/awsctx"
 	"github.com/honeycombio/beeline-go"
 )
 
 func contexter(ctx context.Context, request *awsctx.AwsRequest, inner func(ctx context.Context)) {
 	ctx, span := beeline.StartSpan(ctx, fmt.Sprintf("%s %s", request.Service, request.Action))
+	defer span.Send()
+
 	span.AddField("aws.service", request.Service)
 	span.AddField("aws.action", request.Action)
 
@@ -16,8 +19,12 @@ func contexter(ctx context.Context, request *awsctx.AwsRequest, inner func(ctx c
 		contexterInsertSqsAttributes(ctx, request.Input)
 	}
 
-	defer span.Send()
 	inner(ctx)
+
+	if awsErr, ok := request.Error.(awserr.Error); ok {
+		span.AddField("aws.error.code", awsErr.Code())
+		span.AddField("aws.error.message", awsErr.Message())
+	}
 }
 
 var Contexter = awsctx.ContexterFunc(contexter)
